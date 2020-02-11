98ed7604a24a9b845ad887b2849386b41cf73a33
Mateusz <mateuszd2411@gmail.com>
2020-02-11 09:14:44 +0100

    13

---
+++ app/src/main/java/com/example/instagram/Adapter/MyFotoAdapter.java
@@ -0,0 +1,58 @@
+package com.example.instagram.Adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.instagram.Model.Post;
+import com.example.instagram.R;
+
+import java.util.List;
+
+public class MyFotoAdapter extends RecyclerView.Adapter<MyFotoAdapter.ViewHolder>{
+
+    private Context context;
+    private List<Post> mPosts;
+
+    public MyFotoAdapter(Context context, List<Post> mPost) {
+        this.context = context;
+        this.mPosts = mPost;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(context).inflate(R.layout.fotos_item, viewGroup, false);
+        return new MyFotoAdapter.ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
+        Post post = mPosts.get(i);
+
+        Glide.with(context).load(post.getPostimage()).into(viewHolder.post_image);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mPosts.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        public ImageView post_image;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            post_image = itemView.findViewById(R.id.post_image);
+        }
+    }
+
+}
